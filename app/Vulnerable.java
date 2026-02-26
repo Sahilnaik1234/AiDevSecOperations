fd5582a2242f3f67bd2b0c01c500d7cc831de72d
Sahilnaik1234 <Sahil.naik@triconinfotech.com>
2026-02-26 17:27:59 +0530

    added few errors

--- app/Vulnerable.java
+++ app/Vulnerable.java
@@ -7,9 +7,6 @@ public class Vulnerable {
         // SOC2 Violation: Hardcoded secret (API Key)
         String apiSecret = "sk_test_51MzS0vL3uYfTjSOC2KEY";
         
-        // SOC2 Violation: Insecure Hashing (SHA1)
-        MessageDigest md = MessageDigest.getInstance("SHA1");
-        md.update("secret_payload".getBytes());
 
 
         // SOC2 Violation: Missing audit log for sensitive action
