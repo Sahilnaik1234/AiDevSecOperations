2ee05d9101bbc73052e913adb2f3348e0cadb6cb
Sahilnaik1234 <Sahil.naik@triconinfotech.com>
2026-03-06 18:14:33 +0530

    changed the java file

--- app/Vulnerable.java
+++ app/Vulnerable.java
@@ -1,16 +1,10 @@
-import java.security.MessageDigest;
-import java.net.URL;
-import java.net.HttpURLConnection;
+
 
 public class Vulnerable {
     public static void main(String[] args) throws Exception {
         // SOC2 Violation: Hardcoded secret (API Key)
         String apiSecret = "sk_test_51MzS0vL3uYfTjSOC2KEY";
-        
 
-
-        // SOC2 Violation: Missing audit log for sensitive action
-        deleteUserRecord("user_999");
     }
 
 }
